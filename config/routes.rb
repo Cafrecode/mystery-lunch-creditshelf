e36ff569164b6ce0ce3b269987cbdcadca85aeec
Frederick Nyawaya <o.frederickn@gmail.com>
2020-09-23 21:21:16 +0300

    set up routing for home index page, devise allow for employees to be registered from app, only HR's will be emailed random passwords?

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+    # Required by devise
+  root :to => "home#index"
+
   devise_for :employees
-  resources :employees
+
+  # To manage users outside of devise - basically create users from app
+  scope "/admin" do
+    resources :employees
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
