a12cef2612445233560502ca5ea59d22146371b1
Frederick Nyawaya <o.frederickn@gmail.com>
2020-09-30 05:14:49 +0300

    scheduled matching job

--- Gemfile
+++ Gemfile
@@ -44,6 +44,8 @@ gem 'mailjet'
 
 gem 'solargraph'
 
+gem 'whenever', require: false
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,5 @@
 
 class HomeController < ApplicationController
   def index
-    # probably not the best place for this, but temporary
-    # EmployeeMatchingJob.set(wait_until: 0.days.from_now.end_of_month + 1.second).perform_later
-    EmployeeMatchingJob.perform_later
   end
 end
